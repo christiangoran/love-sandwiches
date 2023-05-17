69135b8f7de1d5fa6edceb7f81eb2b90c2c08ee2
christiangoran <christian.goran@protonmail.com>
2023-05-17 20:26:06 +0200

    Add function to enter surplus data to google worksheet

--- run.py
+++ run.py
@@ -83,6 +83,16 @@ def calculate_surplus_data(sales_row):
         surplus_data.append(surplus)
     return surplus_data
 
+def update_surplus_worksheet(surplus_data):
+    """
+    Update surplus worksheet, add new row with calculated surplus
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(surplus_data)
+    print("Surplus worksheet successully updated. \n")
+
+
 
 def main():
     """
@@ -95,6 +105,7 @@ def main():
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
     print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 print("Welcome to Love Sandwiches Data Automation")
 main()    
